b14016c0dc6cd3c3a08ee152627d1a82adbc30dc
Arnav Pal <palarnav2004@gmail.com>
2026-02-07 15:31:41 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+  name="medical_chatbot",
+  version="0.1.0",
+  author="Arnav Pal",
+  author_email="luciferkill.007@gmail.com",
+  packages=find_packages(),
+  install_requires=[]
+)
